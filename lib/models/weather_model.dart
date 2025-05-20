f7bf477dc5516df136598de80cff4f1b9f3f7d4c
Nikkitha Rosini Amshavel <169536572+Nik-rozx32@users.noreply.github.com>
2025-05-20 14:20:44 +0530

    Create weather_model.dart

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,21 @@
+class Weather {
+  final String cityName;
+  final double temperature;
+  final String description;
+  final String icon;
+
+  Weather(
+      {required this.cityName,
+      required this.temperature,
+      required this.description,
+      required this.icon});
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      cityName: json['name'] ?? 'Unknown City',
+      temperature: (json['main']['temp'] ?? 0).toDouble(),
+      description: json['weather'][0]['description'] ?? 'No description',
+      icon: json['weather'][0]['icon'] ?? '01d',
+    );
+  }
+}
